0f3acb897c530c52f7012a2a17a93306e9512cb8
Mohitjain49 <mohitkjain49@gmail.com>
2025-02-26 19:44:45 -0500

    Setup TS Particles.
    No Content Yet, but this'll do for now.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { loadSlim } from '@tsparticles/slim';
+import VueParticles from '@tsparticles/vue3';
+
 import App from './App.vue'
 import router from './router.js'
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueParticles, {
+    init: async engine => { await loadSlim(engine); },
+}).mount('#app');
--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,3 +1,7 @@
-<template>
+<script setup>
+import { HOME_BACKGROUND } from '../assets/Objects.js';
+</script>
 
+<template>
+<vue-particles id="particlests" :options="HOME_BACKGROUND"></vue-particles>
 </template>
